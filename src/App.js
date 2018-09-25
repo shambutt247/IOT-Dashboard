9744af8def2a6ac7f795869f78d1bfaf9d22e7bd
Rattleball <13022020108@umt.edu.pk>
2018-09-25 23:20:41 +0500

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,69 @@
 import React, { Component } from 'react';
+import Chart from 'chart.js';
 import logo from './logo.svg';
 import './App.css';
 
+import HeaderTopp from "./components/header_topp";
+import MainContent from "./components/main_content";
+
+{/*window.onload = function () {
+
+  var ctx = document.getElementById("myChart");
+  var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+      labels: ["Ja", "Fe", "Ma", "Ap", "Ma", "Ju", "Ju", "Au", "Se", "Oc", "No", "De"],
+      datasets: [{
+        label: "My First dataset",
+        backgroundColor: '#ffffff',
+
+        borderColor: '#1096c1',
+        borderWidth: 5,
+        lineTension: 0,
+        pointRadius: 5,
+        pointHitRadius: 5,
+        pointHoverBackgroundColor: '#53c6ea',
+        pointHoverBorderColor: '#53c6ea',
+        pointHoverBorderWidth: 5,
+        pointHoverRadius: 5,
+        data: [0, 100, 150, 250, 220, 450, 250, 350],
+
+      },
+      {
+        label: "kjhgfd",
+        backgroundColor: '#ffffff',
+
+        borderColor: '#1096c1',
+        borderWidth: 5,
+        lineTension: 0,
+        pointRadius: 5,
+        pointHitRadius: 5,
+        pointHoverBackgroundColor: '#53c6ea',
+        pointHoverBorderColor: '#53c6ea',
+        pointHoverBorderWidth: 5,
+        pointHoverRadius: 5,
+        data: [50, 100, 250, 220, 350],
+      }
+      ]
+    },
+
+    // Configuration options go here
+    options: {}
+  });
+}
+*/}
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <HeaderTopp />
+        <MainContent/>
+
       </div>
     );
   }
